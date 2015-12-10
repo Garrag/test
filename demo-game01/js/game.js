531302a17494bf146b62ef5dce9960bba4097424
Garrag <Garrag_jun@163.com>
2015-12-10 18:55:39 +0800

    游戏demo

---
+++ demo-game01/js/game.js
@@ -0,0 +1,11 @@
+/**
+ * Created by admin on 2015/12/10.
+ */
+var game = {
+   init:function(){
+       $('.gamelayer').hide();
+       $('#gamestartscreen').show();
+
+       game.canvas = $('#gamecanvas')[0];
+   }
+};
