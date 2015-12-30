759dbaa79132674fce84b9897c2eade80d736418
Garrag <Garrag_jun@163.com>
2015-12-30 18:13:02 +0800

    ...

--- demo2-imgtopar/Class.js
+++ demo2-imgtopar/Class.js
@@ -3,11 +3,11 @@
  */
 (function(){
     var initializing = false, fnTest = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /.*/;
-    this.Class = function(){};//定义class(全局变量)
+    this.Class = function(){};
     Class.extend = function(prop) {
-        var _super = this.prototype; //转移本身的原型
+        var _super = this.prototype;
         initializing = true;
-        var prototype = new this(); //用自身构造器创建一个对象,作为新的原型
+        var prototype = new this();
         initializing = false;
         for (var name in prop) {
             prototype[name] = typeof prop[name] == "function" && typeof _super[name] == "function" && fnTest.test(prop[name]) ?
@@ -21,14 +21,14 @@
                     };
                 })(name, prop[name]) : prop[name];
         }
-        function Class() {  //创建一个临时的构造器
+        function Class() {
             if ( !initializing && this.init )
                 this.init.apply(this, arguments);
         }
         Class.prototype = prototype;
         Class.constructor = Class;
-        Class.extend = arguments.callee; //子类也具有改extend方法
-        return Class;//新的进过加工的构造器
+        Class.extend = arguments.callee;
+        return Class;
     };
 })();
 
@@ -36,7 +36,7 @@
 
 
 var a = Class.extend({
-    name:aaa
+    name:"asdfasdfasdfasdfasd"
 });
 
 
